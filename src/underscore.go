c29b91f745b6e2a5f115486fd42c490cbb90c1be
Toby Hede <toby.hede@envato.com>
2014-07-10 20:31:24 +1000

    Reflect all the things

--- src/underscore.go
+++ src/underscore.go
@@ -5,27 +5,158 @@ import (
 	"reflect"
 )
 
+func init() {
+	fmt.Println()
+	MakeContains(&Contains)
+	MakeContains(&StringContains)
+	// MakeMap(&Map)
+	MakeMap(&StringMap)
+	MakeMap(&StringToBoolMap)
+	MakePartition(&PartitionInt)
+}
+
+var Contains func(interface{}, interface{}) bool
+
+var StringContains func([]string, string) bool
+
+// var Map func(interface{}, func(interface{}) interface{}) interface{}
+
+var StringMap func([]string, func(string) string) []string
+
+var StringToBoolMap func([]string, func(string) bool) []bool
+
+var Partition func(interface{}, func(interface{}) bool) interface{}
+
+var PartitionInt func([]int, func(int) bool) ([]int, []int)
+
+var PartitionString func([]string, func(string) bool) []string
+
+
+func MakeX(wrapper interface{}, fn func(args []reflect.Value) (results []reflect.Value)) {
+	wrapperFn := reflect.ValueOf(wrapper).Elem()
+	v := reflect.MakeFunc(wrapperFn.Type(), fn)
+	wrapperFn.Set(v)
+}
+
+func MakeContains(fn interface{}) {
+	MakeX(fn, _contains)
+}
+
+func MakeMap(fn interface{}) {
+	MakeX(fn, _map)
+}
+
+func MakePartition(fn interface{}) {
+	MakeX(fn, _partition)
+}
+
+func _contains(values []reflect.Value) []reflect.Value {
+
+	v := interfaceToValue(values[0])
+	o := values[1].Interface()
+
+	for i := 0; i < v.Len(); i++ {
+		e := v.Index(i).Interface()
+		if e == o {
+			return wrap(reflect.ValueOf(true))
+		}
+	}
+	return wrap(reflect.ValueOf(false))
+}
+
+func _map(values []reflect.Value) []reflect.Value {
+
+	v := interfaceToValue(values[0])
+	fn := values[1]
+
+	out := reflect.SliceOf(fn.Type().Out(0))
+	ret := reflect.MakeSlice(out, v.Len(), v.Len())
+
+	for i := 0; i < v.Len(); i++ {
+
+		e := v.Index(i)
+
+		r := fn.Call([]reflect.Value{e})
+		ret.Index(i).Set(r[0])
+	}
+	return wrap(ret)
+}
+
+
+func _partition(values []reflect.Value) []reflect.Value {
+	slice := values[0]
+	fn := values[1]
+
+	t := reflect.MakeSlice(slice.Type(), 0, 0)
+	f := reflect.MakeSlice(slice.Type(), 0, 0)
+
+	for i := 0; i < slice.Len(); i++ {
+		e := slice.Index(i)
+		r := fn.Call([]reflect.Value{e})
+		if r[0].Bool() {
+			t = reflect.Append(t, e)
+		} else {
+			f = reflect.Append(f, e)
+		}
+	}
+	return []reflect.Value{t, f}
+}
+
+func wrap(v reflect.Value) []reflect.Value {
+	return []reflect.Value{v}
+}
+
+func interfaceToValue(v reflect.Value) reflect.Value {
+	if v.Kind() == reflect.Interface {
+		return reflect.ValueOf(v.Interface())
+	}
+	return v
+}
+
+
+
+
+
+
+
+/// OLD FOLLOWS
+/////////////////////////////////////////////
+
 // type iFn func(interface{}) interface{}
 
+type iterator func(interface{}) interface{}
 
-func Each(slice interface{}, fn func(el interface{}, i int)) {
+// type iteratorWithIndex func(interface{}, int, ...interface{})
+type iteratorWithIndex func(interface{}, int) interface{}
+
+// type iterator fn func(interface{})
+
+func Each(slice interface{}, fn iteratorWithIndex) {
 	s := ToInterface(slice)
 	for i, e := range s {
-		fmt.Println(e)
-		fmt.Println(e)
 		fn(e, i)
 	}
 }
 
-func Contains(slice interface{}, o interface{}) bool {
-	s := ToInterface(slice)
-	for _, e := range s {
-		if e == o {
-			return true
-		}
-	}
-	return false
-}
+// func Map(slice interface{}, fn iterator) []interface{} {
+// 	s := ToInterface(slice)
+
+// 	ret := make([]interface{}, len(s))
+// 	for i, e := range s {
+// 		ret[i] = fn(e)
+// 	}
+// 	return ret
+// }
+
+// func Contains(slice interface{}, o interface{}) bool {
+// 	s := ToInterface(slice)
+// 	for _, e := range s {
+// 		if e == o {
+// 			return true
+// 		}
+// 	}
+// 	return false
+// }
 
 
 func ToInterface(slice interface{}) []interface{} {
--- src/underscore_test.go
+++ src/underscore_test.go
@@ -2,22 +2,23 @@ package __
 
 import (
 	"fmt"
-	"reflect"
 	"testing"
+	"time"
 )
 
+var SLICE = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+
 // func TestEach(t *testing.T) {
+// 	s := []int{1,2,3,4,5}
 
-// 	s := []int{1}
-
+// 	x := 0
 // 	Each(s, func(e interface{}, i int) {
 // 		c := e.(int)
-// 		c++
+// 		x = x + c
 // 		fmt.Println(c)
-// 		fmt.Println(e)
 // 	})
 
-// 	fmt.Println(s)
+// 	fmt.Println(x)
 // }
 
 // func TestContains(t *testing.T) {
@@ -64,16 +65,185 @@ import (
 // }
 
 
-func TestReflect(t *testing.T) {
-	slice := []int{1, 1, 3, 5, 8, 13}
-	s := reflect.ValueOf(&slice)
-	// p := reflect.Pointer(slice)
-	fmt.Println(slice)
-	fmt.Println(s)
-	// v := s.Index(0).Interface()
-	// fmt.Println(&v)
 
-
-
+func contains(slice []string, s string) bool {
+	for _, e := range slice {
+		if e == s {
+			return true
+		}
+	}
+	return false
 }
 
+
+var COUNT = 10000
+
+var in = "z"
+
+
+func with_types() {
+
+	start := time.Now()
+
+	for i := 0; i < COUNT; i++ {
+		contains(SLICE, in)
+	}
+
+	elapsed := time.Since(start)
+	fmt.Println("Typed Contains: ", elapsed)
+}
+
+func with_interface() {
+	start := time.Now()
+
+	for i := 0; i < COUNT; i++ {
+		Contains(SLICE, in)
+	}
+
+	elapsed := time.Since(start)
+	fmt.Println("Interface Contains: ", elapsed)
+}
+
+func with_magic() {
+
+	start := time.Now()
+
+	for i := 0; i < COUNT; i++ {
+		StringContains(SLICE, in)
+	}
+
+	elapsed := time.Since(start)
+	fmt.Println("StringContains: ", elapsed)
+}
+
+
+func TestBench(t *testing.T) {
+	// with_types()
+	// with_interface()
+	// with_magic()
+}
+
+// func TestMap(t *testing.T) {
+// 	mapped := Map(SLICE, func(e interface{}) interface{} {
+// 		s := e.(string)
+// 		return s + "!"
+// 	})
+// 	fmt.Println(mapped)
+// 	// if mapped[0] != "a!" {
+// 	// 	t.Error("First element should be a!")
+// 	// }
+// 	// if mapped[len(mapped)-1] != "z!" {
+// 	// 	t.Error("Last element should be z!")
+// 	// }
+// }
+
+
+func TestContains(t *testing.T) {
+	in := "a"
+	out := "!"
+
+	if b := Contains(SLICE, in); b != true {
+		t.Error("Slice should contain ", in)
+	}
+
+	if b := Contains(SLICE, out); b != false {
+		t.Error("Slice should not contain ", out)
+	}
+}
+
+func TestStringMap(t *testing.T) {
+	fn := func(s string) string {
+		return s + "!"
+	}
+
+	m := StringMap(SLICE, fn)
+
+	if m[0] != "a!" {
+		t.Error("First element should == a!")
+	}
+}
+
+
+func TestStringToBoolMap(t *testing.T) {
+	b := StringToBoolMap(SLICE, func(s string) bool {
+		return s == "z"
+	})
+
+	if b[0]  {
+		t.Error("First element should == false")
+	}
+
+	if !b[len(b)-1]  {
+		t.Error("Last element should == true")
+	}
+}
+
+func partition (slice []int, fn func(int) bool) ([]int, []int) {
+	a := []int{}
+	b := []int{}
+
+	for i := 0; i < len(slice); i++ {
+		e := slice[i]
+		if fn(e) {
+			a = append(a, e)
+		} else {
+			b = append(b, e)
+		}
+	}
+
+	return a, b
+}
+
+func TestPartition(t *testing.T) {
+	slice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	fn := func(i int) bool {
+		return (i % 2) == 1
+	}
+
+	// odd, even := partition(slice, fn)
+	odd, even := PartitionInt(slice, fn)
+
+	fmt.Println(odd)
+	fmt.Println(even)
+
+	if odd[0] != 1  {
+		t.Error("First element should == 1")
+	}
+
+	if even[0] != 2  {
+		t.Error("First element should == 2")
+	}
+}
+
+// func baseIterator(in []reflect.Value) []reflect.Value {
+// 	return []reflect.Value{in[0]}
+// }
+// var intIterator func(int) int
+	// handler := func(fnPtr interface{}) {
+	// 	fmt.Println(fnPtr)
+	// 	fn := reflect.ValueOf(fnPtr).Elem()
+	// 	fmt.Println(fn)
+	// 	v := reflect.MakeFunc(fn.Type(), baseIterator)
+	// 	// Assign it to the value fn represents.
+	// 	fn.Set(v)
+	// }
+	// handler(&intIterator)
+	// fmt.Println("intIterator: ", intIterator(1))
+
+
+	// fn := func(s string) string {
+	// 	return s
+	// }
+	// res := StringMap(slice, fn)
+	// fmt.Println(res)
+	// slice := []int{1, 1, 3, 5, 8, 13}
+	// s := reflect.ValueOf(slice)
+	// p := reflect.Pointer(slice)
+	// fmt.Println(slice)
+	// fmt.Println(s.Index(1))
+	// fmt.Println("slice")
+	// fmt.Println("type of p:", s.Type())
+	// fmt.Println("settability of p:", s.CanSet())
+	// v := s.Index(0).Interface()
+	// fmt.Println(&v)
