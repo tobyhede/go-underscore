cdf9be5390b91890e0f8da13b13038f10d3f9ec9
Toby Hede <toby.hede@envato.com>
2014-08-02 21:11:35 +1000

    None

---
+++ none.go
@@ -0,0 +1,68 @@
+package un
+
+import (
+	"reflect"
+)
+
+func init() {
+	MakeNone(&None)
+	MakeNone(&NoneInt)
+	MakeNone(&NoneString)
+}
+
+// None func(func(A, bool), bool)
+// Returns true if all values in the collection (slice or map) pass the predicate truth test
+
+// var None func(func(value interface{}) bool, interface{}) bool
+var None func(fn, slice_or_map interface{}) bool
+
+// NoneInt
+// Returns true if all values in a []int pass the predicate truth test
+// Predicate function accepts an int and returns a boolean
+var NoneInt func(func(value int) bool, []int) bool
+
+// NoneString
+// Returns true if all values in a []string pass the predicate truth test
+// Predicate function accepts a string and returns a boolean
+var NoneString func(func(value string) bool, []string) bool
+
+// MakeEach implements a typed Each function in the form Each func(func(A, B), []A)
+func MakeNone(fn interface{}) {
+	Maker(fn, none)
+}
+
+func none(values []reflect.Value) []reflect.Value {
+	fn := interfaceToValue(values[0])
+	list := interfaceToValue(values[1])
+
+	var ret bool
+	if list.Kind() == reflect.Map {
+		ret = noneMap(fn, list)
+	}
+
+	if list.Kind() == reflect.Slice {
+		ret = noneSlice(fn, list)
+	}
+
+	return Valueize(reflect.ValueOf(ret))
+}
+
+func noneSlice(fn, s reflect.Value) bool {
+	for i := 0; i < s.Len(); i++ {
+		v := s.Index(i)
+		if ok := predicate(fn, v); ok {
+			return false
+		}
+	}
+	return true
+}
+
+func noneMap(fn, m reflect.Value) bool {
+	for _, k := range m.MapKeys() {
+		v := m.MapIndex(k)
+		if ok := predicate(fn, v); ok {
+			return false
+		}
+	}
+	return true
+}
---
+++ none_test.go
@@ -0,0 +1,69 @@
+package un
+
+import (
+	"strings"
+	"testing"
+)
+
+func init() {
+	// suite("None")
+}
+
+func TestNoneSlice(t *testing.T) {
+
+	fn := func(s interface{}) bool {
+		return false
+	}
+
+	s := ToI([]int{1, 2, 3, 4, 5})
+	result := None(fn, s)
+
+	equals(t, true, result)
+}
+
+func TestNoneMap(t *testing.T) {
+
+	fn := func(s interface{}) bool {
+		return false
+	}
+
+	m := map[string]int{"a": 1, "b": 2, "c": 3, "d": 4, "e": 5}
+	result := None(fn, m)
+
+	equals(t, true, result)
+}
+
+func TestNoneSliceWithInt(t *testing.T) {
+
+	fn := func(i int) bool {
+		return i > 10
+	}
+
+	s := []int{1, 2, 3, 4, 5}
+	result := NoneInt(fn, s)
+
+	equals(t, true, result)
+}
+
+func TestNoneSliceWithString(t *testing.T) {
+
+	fn := func(s string) bool {
+		return strings.Contains(s, "z")
+	}
+
+	s := []string{"a!", "b!", "c!", "d!", "e!"}
+	result := NoneString(fn, s)
+
+	equals(t, true, result)
+}
+
+func TestNoneMapWithInt(t *testing.T) {
+	fn := func(i interface{}) bool {
+		return i.(int) >= 211
+	}
+
+	m := map[string]int{"a": 1, "b": 2, "c": 3, "d": 4, "e": 5}
+	result := None(fn, m)
+
+	equals(t, true, result)
+}
